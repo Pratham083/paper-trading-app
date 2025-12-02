1ada1850a3a22f0151f9e67e23d9d378591989a0
Pratham <prathamp083@gmail.com>
2025-12-02 18:27:13 -0500

    fix fetch stock data

--- backend/src/utils/fetch_stock_data.py
+++ backend/src/utils/fetch_stock_data.py
@@ -2,25 +2,14 @@ import yfinance as yf
 from datetime import datetime, timedelta, timezone
 import pandas as pd
 import numpy as np
-import requests
-
-session = requests.Session()
-session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate',
-    'DNT': '1',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-})
+import time
 
 def safe_round(val):
   return round(val, 2) if isinstance(val, (int, float)) else None
 
 def fetch_stock_details(symbol, max_tries=3):
   for i in range(max_tries):
-    ticker = yf.Ticker(symbol, session=session)
+    ticker = yf.Ticker(symbol)
 
     try:
       info = ticker.info
@@ -42,6 +31,7 @@ def fetch_stock_details(symbol, max_tries=3):
       stock_info['revenue'] = safe_round(info.get('totalRevenue'))
       stock_info['debt'] = safe_round(info.get('totalDebt'))
       break
+    time.sleep(0.1)
 
   return stock_info
 
@@ -62,12 +52,13 @@ def fetch_stock_history(symbol, period, max_tries=3):
   if period not in periods.keys():
     raise ValueError('Not a valid period')
   for i in range(max_tries):
-    ticker = yf.Ticker(symbol, session=session)
+    ticker = yf.Ticker(symbol)
     try:
       hist = ticker.history(period=period, interval=periods[period])
       if hist.empty:
         if i == max_tries - 1:
           raise ValueError("No historical data available.")
+        time.sleep(0.1)
         continue
       results['date'] = list(hist.index.tz_localize(None).strftime("%Y-%m-%d %H:%M:%S").astype(str))
       results['price'] = list(round(hist['Close'],2))
@@ -75,7 +66,9 @@ def fetch_stock_history(symbol, period, max_tries=3):
     except Exception as e:
       if i == max_tries - 1:
           return {'date': [], 'price': []}
+      time.sleep(0.1)
       continue
+    
 
   return results
 
